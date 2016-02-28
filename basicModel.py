ec736abe9177b48c8269928074ce26da14e97b70
JarvisTheJelly <najarvis2016@gmail.com>
2016-02-28 09:39:40 -0500

    Add data for Time/Day

--- basicModel.py
+++ basicModel.py
@@ -4,18 +4,24 @@ AVERAGE_NUM_TRIPS = 3.7845
 # data taken from http://nhts.ornl.gov/tables09/fatcat/2009/avtl_WHYTRP1S.html
 # and http://nhts.ornl.gov/det/Extraction4.aspx
 
-MILEAGE_TYPES = {"LOW": 0,
+MILEAGE_TYPES = {"LOW":    0,
                  "MEDIUM": (MEAN_PER_TRIP - STANDARD_DEVIATION_PER_TRIP) * AVERAGE_NUM_TRIPS,
                  "HIGH":   (MEAN_PER_TRIP + STANDARD_DEVIATION_PER_TRIP) * AVERAGE_NUM_TRIPS}
 # MILEAGE_TYPES are the classifications based on how many miles per day.
 
-TIME_TYPES = {"LOW": 0, "MEDIUM": 2, "HIGH": 4}
-# TIME_TYPES are the classifications based on how many hours per day.
+MEAN_TIME_PER_DAY = 56.09
+STANDARD_DEVIATION_TIME_PER_DAY = 20.0
+# Data taken from http://nhts.ornl.gov/2009/pub/stt.pdf p37
+
+TIME_TYPES = {"LOW":    0,
+              "MEDIUM": MEAN_TIME_PER_DAY - STANDARD_DEVIATION_TIME_PER_DAY,
+              "HIGH":   MEAN_TIME_PER_DAY + STANDARD_DEVIATION_TIME_PER_DAY}
+# TIME_TYPES are the classifications based on how many minutes per day.
 
 TYPE_VALUES = {"LOW": 0, "MEDIUM": 1, "HIGH": 2}
 
 
-def what_type(mileage, time, return_as_string=True):
+def classify(mileage, time, return_as_string=True):
     """Returns the category of driver someone falls into."""
     
     mile_type = time_type = None
@@ -40,11 +46,21 @@ def what_type(mileage, time, return_as_string=True):
         # This should return a different number for each type pair
         return TYPE_VALUES[mile_type] * 3 + TYPE_VALUES[time_type]
     
-    """Format: 
-        
+    """Format: (MILEAGE, TIME) 
+    0 - LOW LOW
+    1 - LOW MEDIUM
+    2 - LOW HIGH
+    3 - MEDIUM LOW
+    4 - MEDIUM MEDIUM
+    5 - MEDIUM HIGH
+    6 - HIGH LOW
+    7 - HIGH MEDIUM
+    8 - HIGH HIGH
     """
 
 if __name__ == "__main__":
-    print what_type(15, 1)
-    print what_type(50, 3)
+    print classify(15, 30, False)
+    print classify(50, 125, False)
+    print classify(15, 30)
+    print classify(50, 125)
 
